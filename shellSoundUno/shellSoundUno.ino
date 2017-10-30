3b0e303e00b739c5d66682a8d03b98f16e4aad06
Jillian <jillianzhong@yahoo.com>
2017-10-30 18:03:58 -0400

    redid layout of files, edited code to work for uno, simplified orientation code

--- shellSoundUno/shellSoundUno.ino
+++ shellSoundUno/shellSoundUno.ino
@@ -3,15 +3,20 @@
    Intel(R) Curie(TM) module can be used to read accelerometer data
    and translate it to an orientation
    
- * Now using Uno and Adafruit accelerometer, adapted 101 example code.
+ * Now using Uno and ADXL335 accelerometer, adapted 101 example code.
+ * using library for ADXL335 https://github.com/codedecaybr/ADXL335
 */
 
 #include "pitches.h"
+#include "ADXL335.h"
 
 // pins for accelerometer
 const int xInput = A0;
 const int yInput = A1;
 const int zInput = A2;
+
+// constructor for accelerometer library
+ADXL335 acc(xInput, yInput, zInput);
  
 // notes in the melody:
 int melody[] = {
@@ -31,6 +36,10 @@ void setup() {
   // initialize device
   Serial.println("Initializing IMU device...");
 
+  // for accelerometer library set up
+  analogReference(EXTERNAL);
+  acc.preset();
+  
   for (int i = 0; i < 14; i++){
    noteDurations[i] = 2;
   }
@@ -38,59 +47,34 @@ void setup() {
 }
 
 void loop() {
+  
   int orientation = -1;   // the board's orientation
   String orientationString; // string for printing description of orientation
-  /*
-   * FROM ARDUINO 101
-    The orientations of the board:
-    0: flat, processor facing up
-    1: flat, processor facing down
-    2: landscape, analog pins down
-    3: landscape, analog pins up
-    4: portrait, USB connector up
-    5: portrait, USB connector down
-  */
+
   // read accelerometer:
-  int x = ReadAxis(xInput);
-  int y = ReadAxis(yInput);
-  int z = ReadAxis(zInput);
+  float x = acc.getGX();
+  float y = acc.getGY();
+  float z = acc.getGZ();
 
-  // calculate the absolute values, to determine the largest
-  int absX = abs(x);
-  int absY = abs(y);
-  int absZ = abs(z);
-
-  if ( (absZ > absX) && (absZ > absY)) {
-    // base orientation on Z
-    if (z > 0) {
-      orientationString = "up";
-      orientation = 0;  
-    } else {
-      orientationString = "down";
-      orientation = 1;
-    }
-  } else if ( (absY > absX) && (absY > absZ)) {
-    // base orientation on Y
-    if (y > 0) {
-      orientationString = "digital pins up";
-      orientation = 2;
-    } else {
-      orientationString = "analog pins up";
-      orientation = 3;
-    }
-  } else {
-    // base orientation on X
-    if (x < 0) {
-      orientationString = "connector up";
-      orientation = 4;
-    } else {
-      orientationString = "connector down";
-      orientation = 5;
-    }
+  // if x, y, z Gvalues are close to 0 (absolute values less than 0.2), 
+  // and are positive, then board is facing up
+  
+  // board facing up
+  if(x <= 0.2 && y <= 0.2 && z <= 0.2 && x > 0 && y > 0 && z > 0){
+    orientation = 0;
+  }
+  // board not facing up
+  else{
+    orientation = 1;
   }
 
-  // send to p5 sketch
-    Serial.println(orientation);
+  Serial.print(x);
+  Serial.print(",");
+  Serial.print(y);
+  Serial.print(",");
+  Serial.print(z);
+  Serial.print(",");
+  Serial.println(orientation);
 
     // if not up orientation, play
     if(orientation != 0 && orientation != -1){
