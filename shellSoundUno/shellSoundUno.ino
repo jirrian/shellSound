0ea958b709a207bd9523f896d31af7b4fe927788
Jillian <jillianzhong@yahoo.com>
2017-10-31 05:01:25 -0400

    accelerometer now compares readings to initial position instead of 0, started adapting VS1053 library example code

--- shellSoundUno/shellSoundUno.ino
+++ shellSoundUno/shellSoundUno.ino
@@ -1,33 +1,62 @@
-/*
- * This sketch example demonstrates how the BMI160 on the
-   Intel(R) Curie(TM) module can be used to read accelerometer data
-   and translate it to an orientation
-   
- * Now using Uno and ADXL335 accelerometer, adapted 101 example code.
+/* 
+ * Sketch for reading orientation of shell through an accelerometer. 
+ * Based on orientation, plays music through VS1053 library and sends orientation to p5 via serial communication.
+ * 
+ * Now using Uno and ADXL335 accelerometer, adapted Arduino 101 accelerometer orientation example code.
  * using library for ADXL335 https://github.com/codedecaybr/ADXL335
+ * 
+ * Adapted code from player_simple example for Adafruit VS1053 Codec Breakout
 */
 
 #include "pitches.h"
 #include "ADXL335.h"
+
+// include SPI, MP3 and SD libraries
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <SD.h>
 
 // pins for accelerometer
 const int xInput = A0;
 const int yInput = A1;
 const int zInput = A2;
 
+// initial positon of accelerometer
+float initx, inity, initz;
+
 // constructor for accelerometer library
 ADXL335 acc(xInput, yInput, zInput);
- 
-// notes in the melody:
-int melody[] = {
-  NOTE_C3, NOTE_C3, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_A3, NOTE_G3, NOTE_F3, NOTE_F3, NOTE_E3, NOTE_E3, NOTE_D3, NOTE_D3, NOTE_C3};
- 
-// note durations: intialize all to quarter notes
-int noteDurations[14];
+
 
 int lastOrientation = - 1; // previous orientation (for comparison)
 
 int noteToPlay = 0;
+
+// for VS1053 breakout board
+// define the pins used
+//#define CLK 13       // SPI Clock, shared with SD card
+//#define MISO 12      // Input data, from VS1053/SD card
+//#define MOSI 11      // Output data, to VS1053/SD card
+// Connect CLK, MISO and MOSI to hardware SPI pins. 
+// See http://arduino.cc/en/Reference/SPI "Connections"
+
+// These are the pins used for the breakout example
+#define BREAKOUT_RESET  9      // VS1053 reset pin (output)
+#define BREAKOUT_CS     10     // VS1053 chip select pin (output)
+#define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
+// These are the pins used for the music maker shield
+#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
+#define SHIELD_CS     7      // VS1053 chip select pin (output)
+#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
+
+// These are common pins between breakout and shield
+#define CARDCS 4     // Card chip select pin
+// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
+#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+
+Adafruit_VS1053_FilePlayer musicPlayer = 
+  // create breakout-example object!
+  Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
 
 void setup() {
   Serial.begin(9600); // initialize Serial communication
@@ -39,6 +68,10 @@ void setup() {
   // for accelerometer library set up
   analogReference(EXTERNAL);
   acc.preset();
+
+  initx = acc.getGX();
+  inity = acc.getGY();
+  initz = acc.getGZ();
   
   for (int i = 0; i < 14; i++){
    noteDurations[i] = 2;
@@ -60,7 +93,7 @@ void loop() {
   // and are positive, then board is facing up
   
   // board facing up
-  if(x <= 0.2 && y <= 0.2 && z <= 0.2 && x > 0 && y > 0 && z > 0){
+  if(abs(x - initx) <= 0.2 && abs(y - inity) <= 0.2 && abs(z - initz) <= 0.2 && x < 0 && y < 0 && z < 0){
     orientation = 0;
   }
   // board not facing up
@@ -68,12 +101,6 @@ void loop() {
     orientation = 1;
   }
 
-  Serial.print(x);
-  Serial.print(",");
-  Serial.print(y);
-  Serial.print(",");
-  Serial.print(z);
-  Serial.print(",");
   Serial.println(orientation);
 
     // if not up orientation, play
